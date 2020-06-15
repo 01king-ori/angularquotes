cc8359b7cc8ff1e90abcee83723ba8998e9611e5
Vincent <chridtinewanja@gmail.com>
2020-06-15 12:11:09 +0300

    This fixes some issues on my code

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,9 +10,9 @@ import { DatePipe } from '@angular/common';
 export class QuotesComponent implements OnInit {
 
   quotes = [
-  new Quotes('Marylin Monroe','I am selfish,impatient and a little insecure.I make mistakes,I am out of control and at times hard to handle.But if you cant handle me at my worst then you sure as hell dont deserve me at my best.','admin', new Date(2020,6,14,22,6,30,30)),
-  new Quotes( 'Oscar Wilde','Be yourself; everyone else is already taken','admin',new Date(2020,6,14,22,6,30,30)),
-  new Quotes('Albert Einstein','Two things are infinite;the universe and human stupidity;and I am not sure about the universe','admin',new Date(2020,6,14,22,6,30,30)), 
+  new Quotes('Marylin Monroe','I am selfish,impatient and a little insecure.I make mistakes,I am out of control and at times hard to handle.But if you cant handle me at my worst then you sure as hell dont deserve me at my best.','admin', new Date(2019,6,14,22,6,30,30)),
+  new Quotes( 'Oscar Wilde','Be yourself; everyone else is already taken','admin',new Date(2019,6,14,22,6,30,30)),
+  new Quotes('Albert Einstein','Two things are infinite;the universe and human stupidity;and I am not sure about the universe','admin',new Date(2019,6,14,22,6,30,30)), 
   
 ]
 
@@ -20,7 +20,7 @@ showDescription= false;//boolean setting to hide and display
 
   addNewQuote(quote) {
     let quoteLength = this.quotes.length;
-    quote.id = quoteLength + 1;
+    quote.name = quoteLength + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
